af990fab58d5bd36a25bb53207187d012884ca8a
xyla <soaproap@gmail.com>
2024-02-16 09:23:56 -0500

    Add basic YouTube support

--- src/components/spiral_overlay.tsx
+++ src/components/spiral_overlay.tsx
@@ -1,19 +1,47 @@
 import * as React from 'react'
-import { useBgImage, useBgImageAlpha } from 'state'
+import {useBgImage, useBgImageAlpha, useMuteOverlay} from 'state'
+
+const ytVideoIDRegex = /^[a-zA-Z0-9]{11}$/
 
 export default function SpiralOverlay () {
   const [url] = useBgImage()
   const [alpha] = useBgImageAlpha()
+  const [muteOverlay] = useMuteOverlay()
+
+  let ytVideoID: string | null = null
+
+  try {
+    const parsedURL = new URL(url)
+
+    // Check if the URL is a YouTube link
+    if ((parsedURL.hostname === 'youtube.com' || parsedURL.hostname === 'www.youtube.com') && parsedURL.pathname === '/watch') {
+      ytVideoID = parsedURL.searchParams.get('v')
+    } else if (parsedURL.hostname === 'youtu.be' || parsedURL.hostname === 'www.youtu.be') {
+      ytVideoID = parsedURL.pathname.split('/')[1]
+    }
+
+    // Only valid YouTube video IDs will be interpreted as a YT video
+    if (ytVideoID !== null && ytVideoIDRegex.test(ytVideoID)) {
+      return <div>
+        <iframe className="spiral_video_overlay"
+                src={`https://www.youtube.com/embed/${ytVideoID}?controls=0&autoplay=1&loop=1&playlist=${ytVideoID}&mute=${muteOverlay ? 1 : 0}`}
+                title="YouTube video player" frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+                style={{opacity: alpha.toString(), pointerEvents: 'none'}}></iframe>
+      </div>
+    }
+  } catch (e) {}
 
   return <div>
-        <div className="spiral_overlay" style={{
-          backgroundImage: `url(${url})`,
-          opacity: alpha.toString()
-        }}></div>
-        <video autoPlay loop className="spiral_video_overlay" key={url} style={{
-          opacity: alpha.toString()
-        }}>
-            <source src={url}/>
-        </video>
-    </div>
+    <div className="spiral_overlay" style={{
+      backgroundImage: `url(${url})`,
+      opacity: alpha.toString()
+    }}></div>
+    <video autoPlay loop muted={muteOverlay} className="spiral_video_overlay" key={url} style={{
+      opacity: alpha.toString()
+    }}>
+      <source src={url}/>
+    </video>
+  </div>
 }
--- src/pages/customize/overlay.tsx
+++ src/pages/customize/overlay.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import { Fragment } from 'react'
-import { useBgImageAlpha, useBgImage } from 'state'
-import { Breadcrumb, FillGap, Label, Page, Slider, TextBlock, TextBox } from 'components/building_blocks'
+import {useBgImageAlpha, useBgImage, useMuteOverlay} from 'state'
+import {Breadcrumb, Checkbox, FillGap, Label, Page, Slider, TextBlock, TextBox} from 'components/building_blocks'
 import Previewer from 'components/previewer'
 import CustomizePage from 'pages/customize'
 
 export default function CustomizeOverlayPage () {
   const [url, setUrl] = useBgImage()
   const [opacity, setOpacity] = useBgImageAlpha()
+  const [muteOverlay, setMuteOverlay] = useMuteOverlay()
 
   return <Fragment>
         <CustomizePage secondary/>
@@ -21,12 +22,14 @@ export default function CustomizeOverlayPage () {
                 overlay opacity
                 <Slider value={opacity} onChange={setOpacity}/>
             </Label>
+            <Checkbox value={muteOverlay} onChange={setMuteOverlay}>Mute Overlay Video</Checkbox>
             <TextBlock medium>
-                The overlay url <b>must</b> link to an image or video <b>file</b>.
-                A proper link usually ends in .png, .jpg, .mp4, etc.
+                The overlay url <b>must</b> link to an image or video <b>file</b>, or to a YouTube video.
+                Some videos cannot be embedded.
+                A proper file link usually ends in .png, .jpg, .mp4, etc.
             </TextBlock>
             <TextBlock>
-                <b>Links to YouTube video pages, imgur albums, etc. will NOT work directly.</b>
+                <b>Links to imgur albums, FA posts, etc. will NOT work directly.</b>
             </TextBlock>
             <TextBlock>
                 <b>If you only want the audio from a video, simply set overlay opacity to 0.00.</b>
