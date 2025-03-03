cc909e8f78b160b74a92671e20c56bcf50595f6a
xyla <soaproap@gmail.com>
2025-03-03 14:02:11 -0500

    Use happy-dom vor vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   test: {
     exclude: [],
-    root: 'src'
-  }
+    root: 'src',
+    environment: 'happy-dom'
+  },
+  plugins: [tsconfigPaths()]
 })
