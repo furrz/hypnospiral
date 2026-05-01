a543a61e5cb92329274736faf3f40399e9175bd1
BlenderB3 <blenderbender3@gmail.com>
2026-05-01 23:01:43 +0100

    Added state tag description Updated repeat tags description to show when using rsvp Refactored tag descriptions to be more maintainable

--- src/features/subliminal/customize/messages.tsx
+++ src/features/subliminal/customize/messages.tsx
@@ -18,17 +18,13 @@ export default function CustomizeSubliminalMessagesPage () {
             <Breadcrumb showInBigPrimary>Subliminal Text</Breadcrumb>
             <CollapsibleSection title="available tags">
                 <ul>
-                    {(!textWall && !rsvp) && <>
-                        <li><Tag tag={'{color:r,g,b}'} min={0} max={255} description="change the text color for the line"/></li>
-                        <li><Tag tag={'{wait:x}'} min={0.000} max={9.999} description="change the duration of the line"/></li>
-                        <li><Tag tag={'{fontScale:x}'} min={0.000} max={9.999} description="change the font scale of the line"/></li>
-                        {!randomOrder && <>
-                            <li><Tag tag={'{begin-repeat}'} description="start a repeating section"/></li>
-                            <li><Tag tag={'{repeat:x}'} min={1} max={99} description="repeat a section x times, starting from the furthest begin-repeat tag"/></li>
-                            </>}
-                        </>}
-                    {!writingMode && !textWall && !rsvp &&
-                        <li><Tag tag={'{write}'} description="this line must be typed out"/></li>}
+                    {(!textWall && (!randomOrder || rsvp)) && <li><Tag tag={'{state:x}'} min={0} max={12} description="change the state of the spiral"/></li>}
+                    {(!textWall && !rsvp) && <li><Tag tag={'{color:r,g,b}'} min={0} max={255} description="change the text color for the line"/></li>}
+                    {(!textWall && !rsvp) && <li><Tag tag={'{wait:x}'} min={0.000} max={9.999} description="change the duration of the line"/></li>}
+                    {(!textWall && !rsvp) && <li><Tag tag={'{fontScale:x}'} min={0.000} max={9.999} description="change the font scale of the line"/></li>}
+                    {(!textWall && (!randomOrder || rsvp)) && <li><Tag tag={'{begin-repeat}'} description="start a repeating section"/></li>}
+                    {(!textWall && (!randomOrder || rsvp)) && <li><Tag tag={'{repeat:x}'} min={1} max={99} description="repeat a section x times, starting from the furthest begin-repeat tag"/></li>}
+                    {(!writingMode && !textWall && !rsvp) && <li><Tag tag={'{write}'} description="this line must be typed out"/></li>}
                     {rsvp && <li><Tag tag={'{speed:x}'} min={60} max={600} description='sets the rate of words per minute at that point, speed will linearly interpolate to the tag from the previous speed tag or starting speed'/></li>}
                     {oneWord && !textWall && !rsvp && <li>for tags to work with single word mode, the tag must be directly next to the word and will not apply to the whole line</li>}
                     {textWall && <li>no tags available for this text mode</li>}
