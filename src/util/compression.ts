f2c2697057dd382fd707ab0ed44e46013cd3ac1d
xyla <soaproap@gmail.com>
2026-05-14 17:04:35 -0400

    Ignore type errors

--- src/util/compression.ts
+++ src/util/compression.ts
@@ -2,11 +2,13 @@ import { readStream } from './streams'
 import 'compression-streams-polyfill'
 
 export async function decompress (data: Uint8Array | string): Promise<Uint8Array> {
+  // @ts-expect-error Blob constructor args are wonky.
   return await readStream(new Blob([data]).stream().pipeThrough(new DecompressionStream('gzip')))
 }
 
 export async function compress (data: Uint8Array | string): Promise<Uint8Array> {
   return await readStream(
+  // @ts-expect-error Blob constructor args are wonky.
     new Blob([data])
       .stream()
       .pipeThrough(new CompressionStream('gzip')))
